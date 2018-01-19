91a0ed75f605fd6c44948afe05559bf428656cd5
Ulrich Werner <ulrichwern@gmail.com>
2018-01-19 00:08:11 -0600

    work

--- src/main/scala/me/ulius/health/controller/FoodLogController.scala
+++ src/main/scala/me/ulius/health/controller/FoodLogController.scala
@@ -11,42 +11,50 @@ import slick.jdbc.PostgresProfile.api._
 
 class FoodLogController(db: Database)
   extends ScalatraServlet
-      with JacksonJsonSupport with CorsSupport with FutureSupport {
+    with JacksonJsonSupport with CorsSupport with FutureSupport {
 
-    private val log =  LoggerFactory.getLogger(getClass)
-    implicit val executor = scala.concurrent.ExecutionContext.Implicits.global
-    protected implicit lazy val jsonFormats: Formats =
-      DefaultFormats.withBigDecimal + new ServingSizeSerializer
+  private val log =  LoggerFactory.getLogger(getClass)
+  implicit val executor = scala.concurrent.ExecutionContext.Implicits.global
 
-  implicit val formats = new DefaultFormats {
-    override def dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  protected implicit lazy val jsonFormats: Formats =
+    DefaultFormats.withBigDecimal + new ServingSizeSerializer
+
+  //  implicit val formats = new DefaultFormats {
+  //    override def dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  //  }
+
+  private val foodTable = TableQuery[FoodTable]
+  private val foodLogTable = TableQuery[FoodLogTable]
+
+  before() {
+    contentType = formats("json")
+    response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
+    response.setHeader("Access-Control-Allow-Origin", "*")
   }
 
-    private val foodlog = TableQuery[FoodLogTable]
-
-    before() {
-      contentType = formats("json")
-      response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
-      response.setHeader("Access-Control-Allow-Origin", "*")
-    }
-
-    options("/*"){
-      response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
-      response.setHeader("Access-Control-Allow-Origin", "*")
-    }
-
-    get("/") {
-      db.run(foodlog.result)
-    }
-
-    post("/") {
-      log.info("hey")
-//      val food = parsedBody.extract[Food]
-//      db.run(foods += food).onComplete(dbio => {
-//        if (dbio.isSuccess) Ok()
-//        if (dbio.isFailure) InternalServerError()
-//      })
-    }
-
+  options("/*"){
+    response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
+    response.setHeader("Access-Control-Allow-Origin", "*")
   }
+
+  get("/") {
+    val blah = for {
+      fl <- foodLogTable
+      f <- foodTable if fl.foodId === f.id
+    } yield (f, fl)
+
+    db.run(blah.result)
+  }
+
+  post("/") {
+    log.info("hey")
+    val foodLog = parsedBody.extract[FoodLog]
+    println(foodLog)
+    db.run(foodLogTable += foodLog).onComplete(dbio => {
+      if (dbio.isSuccess) Ok()
+      if (dbio.isFailure) InternalServerError()
+    })
+  }
+
+}
 
